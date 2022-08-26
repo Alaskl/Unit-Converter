7e6cbf0048bb60b89e6e209d9b8d7a96c9d35c76
Ian <112148110+Alaskl@users.noreply.github.com>
2022-08-26 14:48:59 +0800

    Add files via upload

---
+++ unit.py
@@ -0,0 +1,32 @@
+
+# menu
+
+def print_menu():
+    print('Welcome to this garbage')
+    print('Kilometers to Miles')
+    print('Miles to Kilometers')
+
+#user input (km to mi)
+
+def km_miles():
+    km = float(input('Enter the kilometer value:'))
+    print('Kilometers:'.format(km))
+    miles = km/1.609
+    print(miles)
+
+#user input (mi to km)
+
+def miles_km():
+    miles = float(input('Enter the mile value')) 
+    print('Miles:'.format(miles))
+    km = miles * 1.609
+    print(km)
+
+print_menu()
+
+option = input('choose which conversion you want')
+
+if option == 'A':
+    km_miles()
+if option == 'B':
+    miles_km()
